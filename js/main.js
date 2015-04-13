e14d2c1cf8f3fcec577ec111a7310c645b7ec4b3
Andrey <support@keypoint.ru>
2015-04-13 21:00:30 +0300

    - убрал лишнее в JS коде

--- js/main.js
+++ js/main.js
@@ -10,19 +10,10 @@ $(document).ready(function() {
     // Получим значение верхнего лимита счетчика, где есго остановить
     var limit = $('#clock').data('limit');
 
-    var callbacks_obj = {
-        interval: function() {
-            // Функция срабатывает на каждом временном интервале, при каждой смене чсила
-            var cnt = clock.getTime();
-        }
-    };
-
-
 
     var clock = $('#clock').FlipClock(0, { // Элементы, которым назначается некий функционал в JS, присвоедение объекта,
                                            // им лучше назначать уникальный ID
         clockFace: 'Counter',
-        callbacks: callbacks_obj,
         minimumDigits: 4
     });
 
